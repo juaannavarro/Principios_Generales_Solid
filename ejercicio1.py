8249333eee6ed3c3b7e5c5e6f1b0b4bf1ebd926d
juaannavarro <91721668+juaannavarro@users.noreply.github.com>
2023-09-22 17:07:49 +0200

    eje1

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,6 +1,4 @@
-# Principios_Generales_Solid
-
-Ejercicio práctico1 (3Puntos): Creación de una clase en Python que representa una matriz.
+''' Ejercicio práctico1 (3Puntos): Creación de una clase en Python que representa una matriz.
 Para este ejercicio, deberás crear una clase que representa una matriz. Las operaciones que esta clase debe permitir son la creación de una matriz a partir de una lista de listas, la impresión de la matriz en una forma legible, y el cálculo de la transpuesta de la matriz. Asegúrate de que cada método tenga una única responsabilidad.
 
 
@@ -37,4 +35,10 @@ Se debe evaluar la implementación correcta de la responsabilidad única (cada m
 Criterio de evaluación:
 Claridad y limpieza del código (30%)
 Implementación correcta de la responsabilidad única (40%)
-Uso correcto de las características de Python (30%)
+Uso correcto de las características de Python (30%) '''
+
+Creación de una clase en Python que representa una matriz.
+Para este ejercicio, deberás crear una clase que representa una matriz. Las operaciones que esta clase 
+debe permitir son la creación de una matriz a partir de una lista de listas, la impresión de la matriz 
+en una forma legible, y el cálculo de la transpuesta de la matriz. Asegúrate de que cada método tenga 
+una única responsabilidad.
